e7d961ca41d484e2f12c1cd8bf778eaded5c8c40
sj1218305 <jainshobit0@gmail.com>
2021-06-16 00:25:42 +0530

    init changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import './containers/Tags/index.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="container">
+   <div className="event-container">
+      <nav className="event-categories">
+         <a className="navbar-item">All Events</a>
+         <a className="navbar-item">Webinars</a>
+         <a className="navbar-item">Coding Events</a>
+         <a className="navbar-item">Bootcamp Events</a>
+         <a className="navbar-item">Workshop</a>
+      </nav>
+      <nav className="event-subcategories">
+         <a className="navbar-item">Upcoming</a>
+         <a className="navbar-item">Archived</a>
+         <a className="navbar-item">All Time Favorites</a>
+      </nav>
+      <div className="event-body">
+         <div className="event-cards">
+         </div>
+         <div className="tags">
+            <p>TAGS</p>
+         </div>
+      </div>
+   </div>
+</div>
+);
 }
-
 export default App;
