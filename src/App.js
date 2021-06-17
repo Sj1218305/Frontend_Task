96ef98cbeaff4812cbfc3514f7d38324bb2de3f5
sj1218305 <jainshobit0@gmail.com>
2021-06-17 14:55:17 +0530

    passing category as a parameter

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import './App.css';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Tags from './containers/Tags/index.js'
-
+import Events from './containers/Events/index.js'
+import Test from './containers/Categories/index.js'
 
 function App() {
 return (
-<div className="container">
-   <div className="event-container">
-      <nav className="event-categories">
-         <a className="navbar-item">All Events</a>
-         <a className="navbar-item">Webinars</a>
-         <a className="navbar-item">Coding Events</a>
-         <a className="navbar-item">Bootcamp Events</a>
-         <a className="navbar-item">Workshop</a>
-      </nav>
-      <nav className="event-subcategories">
-         <a className="navbar-item">Upcoming</a>
-         <a className="navbar-item">Archived</a>
-         <a className="navbar-item">All Time Favorites</a>
-      </nav>
-      <div className="event-body">
-         <div className="event-cards">
-         </div>
-         <div className="tags"> 
-            <p>TAGS</p>
-            <Tags />
-         </div>
-      </div>
-   </div>
-</div>
+<Router>  
+  <div className="container">
+     <div className="event-container">
+        <nav className="event-categories">
+           <Link to="/fetchEvents/ALL_EVENTS/" className="navbar-item">All Events</Link>
+           <Link to="/fetchEvents/WEBINARS/" className="navbar-item">Webinars</Link>
+           <Link to="/fetchEvents/CODING_EVENTS/" className="navbar-item">Coding Events</Link>
+           <Link to="/fetchEvents/BOOTCAMP_EVENTS/" className="navbar-item">Bootcamp Events</Link>
+           <Link to="/fetchEvents/WORKSHOP/" className="navbar-item">Workshop</Link>
+        </nav>
+        <nav className="event-subcategories">
+           <Link to="" className="navbar-item">Upcoming</Link>
+           <Link to="" className="navbar-item">Archived</Link>
+           <Link to="" className="navbar-item">All Time Favorites</Link>
+        </nav>
+        <div className="event-body">
+           <div className="event-cards">
+            <Switch>
+              <Route exact path="/fetchEvents/:category" component={Events}/>
+            </Switch>
+           </div>
+           <div className="tags"> 
+              <p>TAGS</p>
+              <Tags />
+           </div>
+        </div>
+     </div>
+  </div>
+</Router>
 );
 }
 export default App;
--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -1,10 +1,18 @@
 import {useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+
+let category = "ALL_EVENTS"
+let sub_category = "Upcoming"
+let tags = []
+let offset = 1
+
 
 const Events = () => {
   // const [fetchTags, setFetchTags] = useState([]); 
-
+  category  = useParams();
+  console.log(category)
   useEffect(() => {
-    fetch(`https://api.codingninjas.com/api/v3/events?event_category=${category}&event_sub_category=${sub_category}&tag_list=${tags}&offset=${offest}`)
+    fetch(`https://api.codingninjas.com/api/v3/events?event_category=${category}&event_sub_category=${sub_category}&tag_list=${tags}&offset=${offset}`)
       .then(res => {
         return res.json();
       })
